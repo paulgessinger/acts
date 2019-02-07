f8671ca42ec2d285ee5d5d3e871d251bd4bbe1f7
Paul Gessinger <paul.gessinger@cern.ch>
2019-02-07 17:11:52 +0100

    Add getters for min max vertex

--- Core/include/Acts/Surfaces/RectangleBounds.hpp
+++ Core/include/Acts/Surfaces/RectangleBounds.hpp
@@ -96,6 +96,11 @@ public:
   double
   halflengthY() const;
 
+  const Vector2D&
+  min() const;
+  const Vector2D&
+  max() const;
+
 private:
   Vector2D m_min;
   Vector2D m_max;
@@ -117,6 +122,18 @@ inline SurfaceBounds::BoundsType
 RectangleBounds::type() const
 {
   return SurfaceBounds::Rectangle;
+}
+
+inline const Vector2D&
+RectangleBounds::min() const
+{
+  return m_min;
+}
+
+inline const Vector2D&
+RectangleBounds::max() const
+{
+  return m_max;
 }
 
 }  // namespace Acts
