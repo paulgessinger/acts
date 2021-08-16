44d26036c914e76dfb836f47613584b08af6e793
Paul Gessinger <hello@paulgessinger.com>
2021-08-16 16:50:25 +0200

    fixes after rebase

--- Examples/Algorithms/TrackFinding/src/SeedingAlgorithm.cpp
+++ Examples/Algorithms/TrackFinding/src/SeedingAlgorithm.cpp
@@ -118,8 +118,8 @@ ActsExamples::ProcessCode ActsExamples::SeedingAlgorithm::execute(
       Acts::SpacePointGridCreator::createGrid<SimSpacePoint>(m_cfg.gridConfig);
   auto spacePointsGrouping = Acts::BinnedSPGroup<SimSpacePoint>(
       spacePointPtrs.begin(), spacePointPtrs.end(), extractGlobalQuantities,
-      bottomBinFinder, topBinFinder, std::move(grid), m_finderCfg);
-  auto finder = Acts::Seedfinder<SimSpacePoint>(m_finderCfg);
+      bottomBinFinder, topBinFinder, std::move(grid), m_cfg.seedFinderConfig);
+  auto finder = Acts::Seedfinder<SimSpacePoint>(m_cfg.seedFinderConfig);
 
   // run the seeding
   static thread_local SimSeedContainer seeds;
