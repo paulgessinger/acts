5e9aa70c7bdba8efaa16875386e11e0fa8689280
Paul Gessinger <hello@paulgessinger.com>
2024-10-12 14:32:26 +0200

    blueprint: add python interface to navigation policies
    requires listing out the types in the binding code, but it gets the job
    done

---
+++ Examples/Python/tests/test_navigation.py
@@ -0,0 +1,46 @@
+import pytest
+
+import acts
+
+import acts.examples
+
+
+def test_navigation_policy_factory():
+
+    policy = (
+        acts.NavigationPolicyFactory()
+        .add(acts.TryAllPortalNavigationPolicy)
+        .add(
+            acts.SurfaceArrayNavigationPolicy,
+            acts.SurfaceArrayNavigationPolicy.Config(
+                layerType=acts.SurfaceArrayNavigationPolicy.LayerType.Plane
+            ),
+        )
+    )
+
+    policy._buildTest()
+
+    policy = (
+        acts.NavigationPolicyFactory()
+        .add(acts.TryAllPortalNavigationPolicy)
+        .add(acts.TryAllSurfaceNavigationPolicy)
+    )
+
+    policy._buildTest()
+
+
+def test_navigation_policy_factory_build_empty():
+    policy = acts.NavigationPolicyFactory()
+
+    with pytest.raises(RuntimeError):
+        policy._buildTest()
+
+
+def test_navigation_policy_factory_add_multiple():
+    with pytest.raises(ValueError):
+        (
+            acts.NavigationPolicyFactory()
+            .add(acts.TryAllPortalNavigationPolicy)
+            .add(acts.TryAllSurfaceNavigationPolicy)
+            .add(acts.TryAllPortalNavigationPolicy)
+        )
